ed38e8a42e5458f75a1effc66ecff5cd9564b896
ampiatt <ampiatt@mail.umkc.edu>
2017-04-02 13:12:50 -0500

    Add files via upload

---
+++ include/rating.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Rating
+{
+    private:
+    int m_cId;
+    int m_rating; 
+    std::string m_bookId;
+    
+    public:
+    Rating();
+    Rating(int id, int rating, std::string book);
+    
+    void setId(int id) {m_cId = id;}
+    void setRating(int rating) {m_rating = rating;}
+    void setBookId(std::string bookId) {m_bookId = bookId;}
+    
+    int getId() {return m_cId;}
+    int getRating() {return m_rating;}
+    std::string getBookId() {return m_bookId;}
+    
+    friend std::istream& operator>>(std::istream& in, Rating& rating);
+};
